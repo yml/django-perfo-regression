f22f20a97c7237f67f8a5f6ad3b720bd6a3cba8e
Yann Malet <yann.malet@gmail.com>
2015-08-04 16:15:54 +0200

    Made the performance issue even worst

--- template_perfo_regression/tests.py
+++ template_perfo_regression/tests.py
@@ -4,7 +4,7 @@ from django.template import RequestContext, loader
 
 class TemplatePerfoTestCase(unittest.TestCase):
     def setUp(self):
-        self.params = {'foo': 'bar'}
+        self.params = {'foo': 'bar', 'range': range(10)}
 
     def rendered_templates(self):
         tmpl = loader.get_template("base.html")
@@ -13,6 +13,5 @@ class TemplatePerfoTestCase(unittest.TestCase):
 
 
     def test_template_performance(self):
-        self.assertEqual(u'btc {0} \n\n'.format(self.params["foo"]),self.rendered_templates())
         timer = timeit.Timer(self.rendered_templates)
         print timer.timeit(1000)
